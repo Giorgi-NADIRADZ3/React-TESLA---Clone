037c74a40c2fd6106cedb749aa003eccd839d3f4
unknown <giorginado10@gmail.com>
2022-03-08 05:07:06 +0400

    deploy my react app to github pages

---
+++ teasla-clone/src/components/Demo.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+
+function App() {
+  return <Button variant="contained">Hello World</Button>;
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
+
+
+export default App;
